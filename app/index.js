82bf7bdf48f56dc5b2750a492795ffd80aaa9c00
ManibabuDevOps <manibabukaranam7@gmail.com>
2025-07-04 19:19:43 +0530

    created app and cf templates

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello World from ECS!');
+}).listen(port);
+
+console.log(`App running at http://localhost:${port}`);
+
